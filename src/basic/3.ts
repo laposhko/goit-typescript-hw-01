04834c3d826e0a7dd74c49bbfa7243385c0ba469
laposhko <laposhko0@gmail.com>
2024-06-06 15:06:37 +0300

    basics

---
+++ src/basic/3.ts
@@ -0,0 +1,3 @@
+// Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type).
+let first: string | number;
+let second: "enable" | "disable";
---
+++ src/basic/5.ts
@@ -0,0 +1,20 @@
+//  Типізуйте функцію isWeekend яка приймає день тижня з enumDayOfWeek і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
+
+enum DayOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+  switch (day) {
+    case DayOfWeek.Saturday | DayOfWeek.Sunday:
+      return false;
+    default:
+      return true;
+  }
+};
